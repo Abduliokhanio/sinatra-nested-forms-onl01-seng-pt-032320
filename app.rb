42e7efe61303867d531b2b96e350ccadc260a1cf
Abdul Khan <akhan17@outlook.com>
2020-05-24 21:51:12 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -13,15 +13,15 @@ module FormsLab
     end
 
     post '/pirates' do
-      @pirate = Pirate.new(params[:pirate])
+          @pirate = Pirate.new(params[:pirate])
 
-      params[:pirate][:ships].each do |details|
-        Ship.new(details)
-      end
-      @ships = Ship.all
+          params[:pirate][:ships].each do |details|
+            Ship.new(details)
+          end
+          @ships = Ship.all
 
-      erb :'pirates/show'
-    end
+          erb :'pirates/show'
+        end
 
   end
 end
